71d62f576b7cbabe24f60f87a89160d4a4d2cbdb
grsnleto <gersonleto7@gmail.com>
2020-02-24 16:40:45 +0700

    Pemrograman Mobile

---
+++ app/src/main/java/com/example/gerson_praktikum_4/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gerson_praktikum_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
